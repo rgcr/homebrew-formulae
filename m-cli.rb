45b8a374ecdd06e0254327fc7c44d7cd4b392839
rgcr <roger.dev@pm.me>
2026-06-01 08:55:14 -0600

    m-cli: remove deprecated no_autobump! and clean up style
    - Remove 'no_autobump! because: :requires_manual_review' (deprecated
      with no replacement; fixes rgcr/m-cli#211).
    - Add Sorbet 'typed: strict' and 'frozen_string_literal: true' sigils.
    - Add class documentation comment.
    - Reorder components per FormulaAudit/ComponentsOrder
      (desc/version/url and caveats/test).
    - Wrap long inreplace lines and switch fish-path regex to %r{...}.
    - Fix heredoc indentation and trailing blank line.
    
    No runtime behavior changes.

--- m-cli.rb
+++ m-cli.rb
@@ -1,13 +1,15 @@
+# typed: strict
+# frozen_string_literal: true
+
+# Homebrew formula for m-cli, the Swiss Army Knife for macOS.
 class MCli < Formula
-  version "2.0.7"
   desc "Swiss Army Knife for macOS"
   homepage "https://github.com/rgcr/m-cli"
   url "https://github.com/rgcr/m-cli/archive/refs/tags/v#{version}.tar.gz"
+  version "2.0.7"
   sha256 "38c55e845ecea36a4b8fad02232014823bd38099c67d2f40b6dbbc4e63a64806"
   license "MIT"
   head "https://github.com/rgcr/m-cli.git", branch: "main"
-
-  no_autobump! because: :requires_manual_review
 
   depends_on :macos
 
@@ -18,8 +20,10 @@ class MCli < Formula
       # Use absolute rather than relative path to plugins.
       s.sub!(/^MCLI_PATH=.+$/, "MCLI_PATH=#{prefix}")
       # Disable options "update" && "uninstall", they must be handled by brew
-      s.sub!(/^\s*update_mcli\s*&&.*/, 'printf "\'m-cli\' was installed by brew, try: brew update && brew upgrade m-cli\n" && exit 0')
-      s.sub!(/^\s*uninstall_mcli\s*&&.*/, 'printf "\'m-cli\' was installed by brew, try: brew uninstall m-cli\n" && exit 0')
+      s.sub!(/^\s*update_mcli\s*&&.*/,
+             'printf "\'m-cli\' was installed by brew, try: brew update && brew upgrade m-cli\n" && exit 0')
+      s.sub!(/^\s*uninstall_mcli\s*&&.*/,
+             'printf "\'m-cli\' was installed by brew, try: brew uninstall m-cli\n" && exit 0')
       s.sub!(/^\s*get_version\s*&&.*/, "printf \"m-cli version: v#{version}\\n\" && exit 0")
     end
 
@@ -35,7 +39,7 @@ class MCli < Formula
 
     inreplace prefix/"completions/fish/m.fish" do |s|
       # Use absolute brew path for fish completion
-      s.gsub!(/^\s*set plugins_dir "\$script_dir\/plugins"$/, "set plugins_dir \"#{prefix}/plugins\"")
+      s.gsub!(%r{^\s*set plugins_dir "\$script_dir/plugins"$}, "set plugins_dir \"#{prefix}/plugins\"")
     end
 
     (prefix/"install.sh").unlink if (prefix/"install.sh").exist?
@@ -46,20 +50,19 @@ class MCli < Formula
     fish_completion.install prefix/"completions/fish/m.fish"
   end
 
+  def caveats
+    <<~EOS
+
+      Some commands are executed with 'sudo' internally because they require root privileges to work correctly.
+      Therefore, make sure you have sudo privileges to use this tool effectively.
+
+    EOS
+  end
+
   test do
     output = pipe_output("#{bin}/m --help 2>&1")
     refute_match(/.*No such file or directory.*/, output)
     refute_match(/.*command not found.*/, output)
     assert_match(/.*Swiss Army Knife for macOS.*/, output)
   end
-
-  def caveats
-    <<~EOS
-
-    Some commands are executed with 'sudo' internally because they require root privileges to work correctly.
-    Therefore, make sure you have sudo privileges to use this tool effectively.
-
-    EOS
-  end
-
 end
